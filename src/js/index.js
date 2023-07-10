4a6ef38b2b119522f17c273ebf1d11c6414fadd5
Geissy-Nascimento <89103088+Geissy-Nascimento@users.noreply.github.com>
2023-07-09 23:31:44 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+//passo 1
+const personagens = document.querySelectorAll('.personagem') 
+
+//passo 2
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450){
+            window.scrollTo({top: onabort, behavior: 'smooth'});
+        }
+
+         //passo 3
+         const personagemSelecionado = document.querySelector('.selecionado');
+         personagemSelecionado.classList.remove('selecionado');
+     
+        personagem.classList.add('selecionado');
+
+        //OBJETIVO 2
+
+        //passo 1
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+        //passo 2
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+
+        //passo 3
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        //passo 4
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+    })   
+})
